de95b294ee3e5fa8e35ff618ec492aaf3e463ac5
Zablocki Tomasz <tomasz.zablocki@capgemini.com>
2015-07-30 09:28:47 +0200

    Commit before chenge repository

--- spring-data-jpa/src/main/java/pl/spring/demo/service/impl/BookServiceImpl.java
+++ spring-data-jpa/src/main/java/pl/spring/demo/service/impl/BookServiceImpl.java
@@ -1,36 +1,43 @@
 package pl.spring.demo.service.impl;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import pl.spring.demo.dao.BookDao;
 import pl.spring.demo.service.BookService;
 import pl.spring.demo.to.BookTo;
 
-import java.util.List;
-
+@Component
 public class BookServiceImpl implements BookService {
+	@Autowired
+	private BookDao bookDao;
 
-    private BookDao bookDao;
+	public BookTo bookk;
 
-    @Override
-    public List<BookTo> findAllBooks() {
-        return bookDao.findAll();
-    }
+	@Override
+	public List<BookTo> findAllBooks() {
+		return bookDao.findAll();
+	}
 
-    @Override
-    public List<BookTo> findBooksByTitle(String title) {
-        return bookDao.findBookByTitle(title);
-    }
+	@Override
+	public List<BookTo> findBooksByTitle(String title) {
+		return bookDao.findBookByTitle(title);
+	}
 
-    @Override
-    public List<BookTo> findBooksByAuthor(String author) {
-        return bookDao.findBooksByAuthor(author);
-    }
+	@Override
+	public List<BookTo> findBooksByAuthor(String author) {
+		return bookDao.findBooksByAuthor(author);
+	}
 
-    @Override
-    public BookTo saveBook(BookTo book) {
-        return bookDao.save(book);
-    }
+	@Override
+	public BookTo saveBook(BookTo book) {
+		bookk = book;
+		return bookDao.save(book);
+	}
 
-    public void setBookDao(BookDao bookDao) {
-        this.bookDao = bookDao;
-    }
+	public void setBookDao(BookDao bookDao) {
+		this.bookDao = bookDao;
+	}
 }
--- spring-data-jpa/src/test/java/pl/spring/demo/service/BookServiceImplTest.java
+++ spring-data-jpa/src/test/java/pl/spring/demo/service/BookServiceImplTest.java
@@ -1,55 +1,59 @@
 package pl.spring.demo.service;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+
+import java.util.List;
+
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
 import pl.spring.demo.exception.BookNotNullIdException;
 import pl.spring.demo.to.BookTo;
-
-import java.util.List;
-
-import static org.junit.Assert.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "CommonServiceTest-context.xml")
 public class BookServiceImplTest {
 
-    @Autowired
-    private BookService bookService;
+	@Autowired
+	private BookService bookService;
 
-    @Test
-    public void testShouldFindAllBooks() {
-        // when
-        List<BookTo> allBooks = bookService.findAllBooks();
-        // then
-        assertNotNull(allBooks);
-        assertFalse(allBooks.isEmpty());
-        assertEquals(6, allBooks.size());
-    }
+	@Test
+	public void testShouldFindAllBooks() {
+		// when
+		List<BookTo> allBooks = bookService.findAllBooks();
+		// then
+		assertNotNull(allBooks);
+		assertFalse(allBooks.isEmpty());
+		assertEquals(6, allBooks.size());
+	}
 
-    @Test
-    @Ignore
-    public void testShouldFindAllBooksByTitle() {
-        // given
-        final String title = "Opium w rosole";
-        // when
-        List<BookTo> booksByTitle = bookService.findBooksByTitle(title);
-        // then
-        assertNotNull(booksByTitle);
-        assertFalse(booksByTitle.isEmpty());
-    }
+	@Test
+	@Ignore
+	public void testShouldFindAllBooksByTitle() {
+		// given
+		final String title = "Opium w rosole";
+		// when
+		List<BookTo> booksByTitle = bookService.findBooksByTitle(title);
+		// then
+		assertNotNull(booksByTitle);
+		assertFalse(booksByTitle.isEmpty());
+	}
 
-    @Test(expected = BookNotNullIdException.class)
-    public void testShouldThrowBookNotNullIdException() {
-        // given
-        final BookTo bookToSave = new BookTo();
-        bookToSave.setId(22L);
-        // when
-        bookService.saveBook(bookToSave);
-        // then
-        fail("test should throw BookNotNullIdException");
-    }
+	@Test(expected = BookNotNullIdException.class)
+	public void testShouldThrowBookNotNullIdException() {
+		// given
+		final BookTo bookToSave = new BookTo();
+		bookToSave.setId(22L);
+		// when
+		bookService.saveBook(bookToSave);
+		// then
+		fail("test should throw BookNotNullIdException");
+	}
 }
